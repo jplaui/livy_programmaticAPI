a93423fa078cb1169cb1ebf5cb244c0bd92713a7
jan <jan-lauinger@gmx.de>
2017-01-12 18:01:19 +0100

    update README.md

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # livy_programmaticAPI
 java implementation of livy programmatic API
+
+#project does not work at the moment
+
+## Usage:
+- install Apache Maven
+- clone this repository
+- follow the next instructions
+
+### instructions:
+to build the jar for the upload change into the jarJob project directory
+```sh
+$ cd livy_programmaticAPI
+$ cd jarJob
+```
+build the jar(for the upload) with maven:
+in the jarJob directory type
+```sh
+$ mvn clean install
+```
+this creates the jarJob-1.jar jar-File into the livy_programmaticAPI/jar-Job/target folder
+
+now use the *cd* command and switch into the mainModule folder and type
+```sh
+$ mvn compile
+$ mvn exec:java -Dexec.mainClass="groupIDmainModule.App"
+```
+this still provokes an error.
--- mainModule/src/main/java/groupIDmainModule/App.java
+++ mainModule/src/main/java/groupIDmainModule/App.java
@@ -20,40 +20,39 @@ public class App
         LivyClient client = null;
         LivyClientBuilder builder = null;
         try {
-			builder = new LivyClientBuilder();
-       		try {
-       			builder.setURI(new URI(livyUrl));
-       		}
-			catch(java.net.URISyntaxException ex){
-				ex.printStackTrace();
-			}
-		}
-		catch(IOException e){
-  			e.printStackTrace();
-		}
+            builder = new LivyClientBuilder();
+            try {
+                builder.setURI(new URI(livyUrl));
+            }
+            catch(java.net.URISyntaxException ex){
+                ex.printStackTrace();
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
-		client = builder.build();
+        client = builder.build();
 
-		try {
-			System.err.printf("Uploading %s to the Spark context...\n", piJar);
-			client.uploadJar(new File(piJar)).get();
+        try {
+            System.err.printf("Uploading %s to the Spark context...\n", piJar);
+            client.uploadJar(new File(piJar)).get();
 
-			System.err.printf("Running PiJob with %d samples...\n", samples);
-			double pi = client.submit(new PiJob(samples)).get();
+            System.err.printf("Running PiJob with %d samples...\n", samples);
+            double pi = client.submit(new PiJob(samples)).get();
 
-			System.out.println("Pi is roughly: " + pi);
-		}
-		catch(java.lang.InterruptedException ex) {
-			ex.printStackTrace();
-		}
-		catch(java.util.concurrent.ExecutionException ex) {
-			ex.printStackTrace();
-		} finally {
-			client.stop(true);
-		}
+            System.out.println("Pi is roughly: " + pi);
+        }
+        catch(java.lang.InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        catch(java.util.concurrent.ExecutionException ex) {
+            ex.printStackTrace();
+        } finally {
+            client.stop(true);
+        }
     }
 }
-
 
 // execute this file by cd into mainModule dir. then execute 
 // this command: mvn exec:java -Dexec.mainClass="groupIDmainModule.App"
